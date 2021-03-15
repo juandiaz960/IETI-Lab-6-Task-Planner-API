c695e421d854675d77f67ea0dc7c6b0de49e24b0
juandiaz960 <42452344+juandiaz960@users.noreply.github.com>
2021-03-15 11:19:56 -0500

    Spring Project

---
+++ src/main/java/com/ieti/TaskPlanner/TaskPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ieti.TaskPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerApiApplication.class, args);
+	}
+
+}
